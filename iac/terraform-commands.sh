a5cf5991b5839af90989600ea824516d26909dc4
Elad Tal <eladt.pro@gmail.com>
2021-12-08 10:40:12 +0200

    auto apply

--- iac/terraform-commands.sh
+++ iac/terraform-commands.sh
@@ -5,7 +5,7 @@ terraform apply -auto-approve   # Deploy
 terraform show # show the curremt state of the resources terraform knows about
 
 terraform plan -var "rgName=rg-saga"
-terraform apply -var "rgName=rg-saga"
+terraform apply -var "rgName=rg-saga" -auto-approve
 
 # find extension "type handler version"
 #az vm extension image list --location westus -o table
