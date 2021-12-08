05f923189fd8caf132ee4a1bba376d858a521f26
Elad Tal <eladt.pro@gmail.com>
2021-12-08 10:41:09 +0200

    az login

--- iac/terraform-commands.sh
+++ iac/terraform-commands.sh
@@ -4,6 +4,7 @@ terraform plan  # Plan the deployment
 terraform apply -auto-approve   # Deploy
 terraform show # show the curremt state of the resources terraform knows about
 
+az login
 terraform plan -var "rgName=rg-saga"
 terraform apply -var "rgName=rg-saga" -auto-approve
 
